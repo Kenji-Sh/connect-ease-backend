dc12ca173bafb426172a8580aa56884a8615d017
Kenji <jmkenjisb@outlook.com>
2024-06-26 01:29:24 -0300

    feat: create base method for create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { CreateUserDTO } from './dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  async create(
+    createUserDTO: CreateUserDTO,
+  ): Promise<User> {
+    const createdUser = new this.userModel(
+      createUserDTO,
+    );
+    return createdUser.save();
+  }
+}
